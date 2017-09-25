d7c917dd225448924ddf3c87115951ed43e6dd67
Patrick Kyle Garcia <pkmgarcia@yahoo.com>
2017-09-25 11:33:40 -0700

    init vue and express apps

---
+++ express-mini-safeway/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = Express()
+
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.listen(process.env.PORT || 3000, function() {
+   console.log("Hello world")
+   }
+)
