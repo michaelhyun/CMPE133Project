b4397f9a911121a16b9d0fd7af7dd7220f8c5a5c
Patrick Kyle Garcia <pkmgarcia@yahoo.com>
2017-10-11 14:49:59 -0700

    Added more boilerplate

--- mini-safeway/src/views/items/productpage.vue
+++ mini-safeway/src/views/items/productpage.vue
@@ -1,9 +1,15 @@
 <template>
+
+  <v-content>
+    <v-container fluid fill-height>
 	<!--
 	<header></header>
 	<image></image>
 	<prodpagedesc></prodpagedesc>
 	-->
+	<span>SOMETHING</span>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
